f3c4ae889e50a91798c74fdd3182e848415efdef
HeatherAckerman <hla20115213@gmail.com>
2021-01-04 13:50:32 -0600

    first question is displaying after clicking start

---
+++ questions.js
@@ -0,0 +1,53 @@
+// Questions courtesy of: https://thoughtcatalog.com/kelly-peacock/2020/04/multiple-choice-trivia-questions-and-answers/
+var questions = [
+    {
+        title: "How many blue stripes are on the US flag?",
+        choices: ["6", "7", "13", "0"],
+        answer: "0"
+    },
+    {
+        title: "What is the color of Donald Duck's bowtie?",
+        choices: ["Red", "Yellow", "Blue", "White"],
+        answer: "red"
+    },
+    {
+        title: "Which animal does not appear in the chinese zodiac?",
+        choices: ["Dragon", "Rabbit", "Dog", "Hummingbird"],
+        answer: "Hummingbird"
+    },
+    {
+        title: "Which planet is the hottest?",
+        choices: ["Venus", "Saturn", "Mercury", "Mars"],
+        answer: "Venus"
+    },
+    {
+        title: "In which city can  you find the Libety Bell?",
+        choices: ["Washington DC", "Boston", "Philadelphia", "Manhatten"],
+        answer: "Philidelphia0"
+    },
+    {
+        title: "According to Forrest Gump, “life was like…”?",
+        choices: ["a bag of lemons", "a handful of roses", "a Lollipop", "a box of chocolates"],
+        answer: "A box of chocolates"
+    },
+    {
+        title: "What is the rarest blood type?",
+        choices: ["AB-Negative", "B-Negative", "A-Positive", "O-Positive"],
+        answer: "AB-Negative"
+    },
+    {
+        title: "Which U.S. state is known as the sunflower state?",
+        choices: ["Florida", "Kansas", "California", "Maine"],
+        answer: "Kansas"
+    },
+    {
+        title: "Fe is the chemical symbol for…?",
+        choices: ["Iron", "Fluorine", "Hydrogen", "Zinc"],
+        answer: "Iron"
+    },
+    {
+        title: "What language is the most spoken worldwide?",
+        choices: ["Spanish", "Chinese", "Arabic", "English"],
+        answer: "Chinese"
+    },
+]
--- script.js
+++ script.js
@@ -1,139 +1,36 @@
-// Questions courtesy of: https://thoughtcatalog.com/kelly-peacock/2020/04/multiple-choice-trivia-questions-and-answers/
-var qAndA = [
-    {
-        q: "How many blue stripes are on the US flag?",
-        choices: ["6", "7", "13", "0"],
-        a: "0"
-    },
-    {
-        q: "What is the color of Donald Duck's bowtie?",
-        choices: ["Red", "Yellow", "Blue", "White"],
-        a: "red"
-    },
-    {
-        q: "Which animal does not appear in the chinese zodiac?",
-        choices: ["Dragon", "Rabbit", "Dog", "Hummingbird"],
-        a: "Hummingbird"
-    },
-    {
-        q: "Which planet is the hottest?",
-        choices: ["Venus", "Saturn", "Mercury", "Mars"],
-        a: "Venus"
-    },
-    {
-        q: "In which city can  you find the Libety Bell?",
-        choices: ["Washington DC", "Boston", "Philadelphia", "Manhatten"],
-        a: "Philidelphia0"
-    },
-    {
-        q: "According to Forrest Gump, “life was like…”?",
-        choices: ["a bag of lemons", "a handful of roses", "a Lollipop", "a box of chocolates"],
-        a: "A box of chocolates"
-    },
-    {
-        q: "What is the rarest blood type?",
-        choices: ["AB-Negative", "B-Negative", "A-Positive", "O-Positive"],
-        a: "AB-Negative"
-    },
-    {
-        q: "Which U.S. state is known as the sunflower state?",
-        choices: ["Florida", "Kansas", "California", "Maine"],
-        a: "Kansas"
-    },
-    {
-        q: "Fe is the chemical symbol for…?",
-        choices: ["Iron", "Fluorine", "Hydrogen", "Zinc"],
-        a: "Iron"
-    },
-    {
-        q: "What language is the most spoken worldwide?",
-        choices: ["Spanish", "Chinese", "Arabic", "English"],
-        a: "Chinese"
-    },
-]
+// variables to keep track of quiz state
+var currentQuestionIndex = 0;
 
-var timers = document.getElementById("timers");
-var questionsDisplay = document.getElementById("questionsDisplay");
-var a = document.getElementById("a");
-var b = document.getElementById("b");
-var c = document.getElementById("c");
-var d = document.getElementById("d");
-var score = 0;
-var showScore = document.getElementById("showScore");
-showScore.textContent = "Score: " + score;
-var startButton = document.getElementById("startButton");
-var lastScore = localStorage.getItem("initials");
-var lastInitials = localStorage.getItem("score");
-var message = document.getElementById("message")
+var questionsEl = document.getElementById("questions");
+var choicesE1 = document.getElementById("choices");
+var startButton = document.getElementById("start");
 
+function go() {
+  // hide start screen
+  var startScreenE1 = document.getElementById("start-screen");
+  startScreenE1.setAttribute("class", "hide");
 
+  // un-hide questions section
+  questionsEl.removeAttribute("class");
 
-function prepare() {
-
-    var secondsLeft = 3;
-    var timeInterval = setInterval(function () {
-
-        timers.textContent = "Starting in " + secondsLeft;
-        secondsLeft--;
-
-        if (secondsLeft === 0 || secondsLeft < 0) {
-
-            clearInterval(timeInterval);
-            timer();
-            firstQuestion();
-        }
-    }, 1000);
-
+  showQuestions();
 };
 
 
 secondsLeft = 60;
 
-function timer() {
-    var timerInterval = setInterval(function () {
 
-        timers.textContent = secondsLeft + " Seconds remaining";
-        secondsLeft--;
+function showQuestions() {
+      // get current question object from array
+      var currentQuestion = questions[currentQuestionIndex];
 
-        if (secondsLeft === 0 || secondsLeft < 0) {
-            clearInterval(timerInterval);
-            timers.textContent = " ";
-            q.textContent = " ";
-            a.textContent = " ";
-            b.textContent = " ";
-            c.textContent = " ";
-            d.textContent = " ";
-            displayResults();
-        }
-    }, 1000);
+  // update title with current question
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = currentQuestion.title;
 
 }
 
-function firstQuestion() {
-    questionsDisplay.textContent = qAndA[0].q;
-    a.textContent = qAndA[0].a;
-    b.textContent = qAndA[0].b;
-    c.textContent = qAndA[0].c;
-    d.textContent = qAndA[0].d;
 
-    a.addEventListener("click", function () {
-        secondsLeft--;
-
-    });
-    b.addEventListener("click", function () {
-        secondsLeft--;
-
-    });
-    c.addEventListener("click", function () {
-        secondsLeft--;
-
-    });
-    d.addEventListener("click", function () {
-        score++;
-        showScore.textContent = score;
-        secondQuestion();
-    });
-}
 
 
 function waitForUser() {
@@ -153,5 +50,6 @@ function saveResults() {
     }
 }
 
-startButton.addEventListener("click", prepare);
+// user clicks button to start quiz
+startButton.onclick = go;
 
