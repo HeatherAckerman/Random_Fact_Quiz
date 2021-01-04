d0d7c59edc3a0e66e97d6fe4dec519257515955f
HeatherAckerman <hla20115213@gmail.com>
2021-01-04 16:03:39 -0600

    added choices, timer, and highscores

--- questions.js
+++ questions.js
@@ -1,52 +1,52 @@
 // Questions courtesy of: https://thoughtcatalog.com/kelly-peacock/2020/04/multiple-choice-trivia-questions-and-answers/
-var questions = [
+var qAndA = [
     {
-        title: "How many blue stripes are on the US flag?",
+        question: "How many blue stripes are on the US flag?",
         choices: ["6", "7", "13", "0"],
         answer: "0"
     },
     {
-        title: "What is the color of Donald Duck's bowtie?",
+        question: "What is the color of Donald Duck's bowtie?",
         choices: ["Red", "Yellow", "Blue", "White"],
         answer: "red"
     },
     {
-        title: "Which animal does not appear in the chinese zodiac?",
+        question: "Which animal does not appear in the chinese zodiac?",
         choices: ["Dragon", "Rabbit", "Dog", "Hummingbird"],
         answer: "Hummingbird"
     },
     {
-        title: "Which planet is the hottest?",
+        question: "Which planet is the hottest?",
         choices: ["Venus", "Saturn", "Mercury", "Mars"],
         answer: "Venus"
     },
     {
-        title: "In which city can  you find the Libety Bell?",
+        question: "In which city can  you find the Libety Bell?",
         choices: ["Washington DC", "Boston", "Philadelphia", "Manhatten"],
         answer: "Philidelphia0"
     },
     {
-        title: "According to Forrest Gump, “life was like…”?",
+        question: "According to Forrest Gump, “life was like…”?",
         choices: ["a bag of lemons", "a handful of roses", "a Lollipop", "a box of chocolates"],
         answer: "A box of chocolates"
     },
     {
-        title: "What is the rarest blood type?",
+        question: "What is the rarest blood type?",
         choices: ["AB-Negative", "B-Negative", "A-Positive", "O-Positive"],
         answer: "AB-Negative"
     },
     {
-        title: "Which U.S. state is known as the sunflower state?",
+        question: "Which U.S. state is known as the sunflower state?",
         choices: ["Florida", "Kansas", "California", "Maine"],
         answer: "Kansas"
     },
     {
-        title: "Fe is the chemical symbol for…?",
+        question: "Fe is the chemical symbol for…?",
         choices: ["Iron", "Fluorine", "Hydrogen", "Zinc"],
         answer: "Iron"
     },
     {
-        title: "What language is the most spoken worldwide?",
+        question: "What language is the most spoken worldwide?",
         choices: ["Spanish", "Chinese", "Arabic", "English"],
         answer: "Chinese"
     },
--- script.js
+++ script.js
@@ -1,55 +1,161 @@
 // variables to keep track of quiz state
 var currentQuestionIndex = 0;
+var time = 60;
+var timerId;
 
-var questionsEl = document.getElementById("questions");
-var choicesE1 = document.getElementById("choices");
+// variables to reference DOM elements
+var qAndAEl = document.getElementById("qAndA");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
+var submitButton = document.getElementById("submit");
 var startButton = document.getElementById("start");
+var initialsEl = document.getElementById("initials");
+var feedbackEl = document.getElementById("feedback");
 
-function go() {
-  // hide start screen
-  var startScreenE1 = document.getElementById("start-screen");
-  startScreenE1.setAttribute("class", "hide");
+function startQuiz() {
+    // hide start screen
+    var startScreenEl = document.getElementById("startScreen");
+    startScreenEl.setAttribute("class", "hide");
 
-  // un-hide questions section
-  questionsEl.removeAttribute("class");
+    // un-hide qAndA section
+    qAndAEl.removeAttribute("class");
 
-  showQuestions();
-};
+    // start timer
+    timerId = setInterval(updateTime, 1000);
 
+    // show starting time
+    timerEl.textContent = time;
 
-secondsLeft = 60;
-
-
-function showQuestions() {
-      // get current question object from array
-      var currentQuestion = questions[currentQuestionIndex];
-
-  // update title with current question
-  var titleEl = document.getElementById("question-title");
-  titleEl.textContent = currentQuestion.title;
-
+    getQuestion();
 }
 
+function getQuestion() {
+    // get current question object from array
+    var currentQuestion = qAndA[currentQuestionIndex];
 
+    // update question area with current question
+    var questionEl = document.getElementById("question");
+    questionEl.textContent = currentQuestion.question;
 
+    // clear out any old question choices
+    choicesEl.innerHTML = "";
 
-function waitForUser() {
-    message.textContent = "You scored: " + score + "The last recorded score was: " + lastScore + "Scored by: " + lastInitials + " To save your score enter your initials in the empty box and press save."
+    // loop over choices
+    currentQuestion.choices.forEach(function (choice, i) {
+        // create new button for each choice
+        var choiceNode = document.createElement("button");
+        choiceNode.setAttribute("class", "choice");
+        choiceNode.setAttribute("value", choice);
 
-    save.addEventListener("click", saveResults);
+        choiceNode.textContent = choice;
 
+        // attach click event listener to each choice
+        choiceNode.onclick = choiceClick;
+
+        // display on the page
+        choicesEl.appendChild(choiceNode);
+    });
 }
 
-function saveResults() {
-    if (initials === "") {
-        localStorage.setItem("initials", initials);
-        localStorage.setItem("score", score);
-        alert("Score Saved!")
+function choiceClick() {
+    // check if user guessed wrong
+    if (this.value !== qAndA[currentQuestionIndex].answer) {
+        // penalize time for wrong answer
+        time -= 5;
+
+        if (time < 0) {
+            time = 0;
+        }
+
+        // display new time on page
+        timerEl.textContent = time;
+        feedbackEl.textContent = "Wrong!";
     } else {
-        alert("Try agian");
+        time += 5;
+        feedbackEl.textContent = "Correct!";
+    }
+
+    // flash right/wrong feedback on page for half a second
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function () {
+        feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+
+    // move to next question
+    currentQuestionIndex++;
+
+    // check if we've run out of qAndA
+    if (currentQuestionIndex === qAndA.length) {
+        quizEnd();
+    } else {
+        getQuestion();
     }
 }
 
+function quizEnd() {
+    // stop timer
+    clearInterval(timerId);
+
+    // show end screen
+    var endScreenEl = document.getElementById("end-screen");
+    endScreenEl.removeAttribute("class");
+
+    // show final score
+    var finalScoreEl = document.getElementById("final-score");
+    finalScoreEl.textContent = time;
+
+    // hide qAndA section
+    qAndAEl.setAttribute("class", "hide");
+}
+
+function updateTime() {
+    // update time
+    time--;
+    timerEl.textContent = time;
+
+    // check if user ran out of time
+    if (time <= 0) {
+        quizEnd();
+    }
+}
+
+function saveHighscore() {
+    // get value of input box
+    var initials = initialsEl.value.trim();
+
+    // make sure value wasn't empty
+    if (initials !== "") {
+        // get saved scores from localstorage, or if not any, set to empty array
+        var highscores =
+            JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+        // format new score object for current user
+        var newScore = {
+            score: time,
+            initials: initials
+        };
+
+        // save to localstorage
+        highscores.push(newScore);
+        window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+        // redirect to next page
+        window.location.href = "highscores.html";
+    }
+}
+
+function checkForEnter(event) {
+    if (event.key === "Enter") {
+        saveHighscore();
+    }
+}
+
+// user clicks button to submit initials
+submitButton.onclick = saveHighscore;
+
 // user clicks button to start quiz
-startButton.onclick = go;
+startButton.onclick = startQuiz;
+
+initialsEl.onkeyup = checkForEnter;
+
 
